5f964b8d0e9035b29230dbea5d5526b64bf75129
Rodrigo <h2bits@gmail.com>
2024-08-12 15:35:48 +0000

    Commit inicial

---
+++ conta-banco/src/ContaTerminal.java
@@ -0,0 +1,11 @@
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        // TODO: Conhecer e importar a classe Scanner
+
+        // TODO: Exibir as mensagens para o usuário
+
+        // TODO: Obter os valores digitados no terminal usando Scanner
+
+        // TODO: Exibir a mensagem conta criada
+    }
+}
